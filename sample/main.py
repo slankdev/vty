28a1e610cc01c2cb3071107209560019fb734f2f
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-03-28 09:47:54 +0000

    2018.03.28-09:47

--- sample/main.py
+++ sample/main.py
@@ -3,11 +3,11 @@ import telnetlib
 from telnetlib import Telnet
 
 tn = Telnet('localhost', 2605)
-# tn.read_until(b'Password: ')
-# tn.write(b'zebra\n')
-tn.read_until('router> ')
+tn.read_until(b'Password: ')
+tn.write(b'zebra\n')
+tn.expect(['.*> '])
 tn.write('show memory\n')
-res = tn.read_until('router> ')
+res = tn.expect(['.*> '])[2]
 tn.write(b'exit\n')
 
 tL = res.split('\n')[1:-1]
